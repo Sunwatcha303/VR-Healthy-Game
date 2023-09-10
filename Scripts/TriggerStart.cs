dffd1b36ce356f1a91a832502d29d92df2bb8697
Sunwatcha303 <watcharakorn301146@gmail.com>
2023-09-10 23:49:13 +0700

    (fix)edit-script: edit

--- Scripts/TriggerStart.cs
+++ Scripts/TriggerStart.cs
@@ -27,8 +27,6 @@ public class TriggerStart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
         if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) && Time.time > nextClick)
         {
             nextClick = Time.time + clickRate;
